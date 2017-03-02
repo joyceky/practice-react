45ae4231e639e57d584404df8cd9de4844a5b2af
Kyle Joyce <kyyjoyce@gmail.com>
2017-03-01 17:50:46 -0700

    youtube api practice

--- src/components/app.js
+++ src/components/app.js
@@ -3,26 +3,52 @@ import YTSearch from 'youtube-api-search';
 
 const API_KEY = 'AIzaSyBO8ZHNa6_L78CefGExR3UD5TBOFa7lpoI';
 
-
 export default class App extends Component {
-  // constructor() {
-  //   super();
-  //
-  // }
+  constructor() {
+    super();
+
+    this.state = {
+      videos: [],
+      color: "black"
+    };
+
+    this.videoSearch = this.videoSearch.bind(this);
+    this.handleButtonClick = this.handleButtonClick.bind(this);
+  }
 
   componentDidMount() {
-    console.log(this.videoSearch('kitties'));
+    this.videoSearch('kitties');
   }
 
   videoSearch(term) {
-    YTSearch({key: API_KEY, term: term}, (videos) => {
+    YTSearch({key: API_KEY, term }, (videos) => {
       console.log(videos);
+      this.setState({ videos });
     });
+  }
+
+  handleButtonClick() {
+    this.setState({ color: 'blue' });
   }
 
   render() {
     return (
-      <div>React simple starter</div>
+      <div>
+        <SubmitButton color={this.state.color} handleButtonClick={this.handleButtonClick} />
+        {this.state.videos.map((video, i) => {
+          return <p key={i}>{video.snippet.title}</p>
+        })}
+      </div>
     );
   }
 }
+
+const SubmitButton = ({ handleButtonClick, color }) => {
+  return (
+    <button
+      onClick = {handleButtonClick}
+      style={{backgroundColor: color}}>
+      Submit
+    </button>
+  );
+};
